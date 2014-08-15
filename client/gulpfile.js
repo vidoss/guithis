dcd73adff7271b0150597e5541f4f5775e8247b6
vidoss <victor.doss@gmail.com>
2014-08-15 12:03:25 -0700

    Added gulp watch.

---
+++ client/gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+	 rename = require('gulp-rename'),
+	 dust = require('gulp-dust'),
+	 browserify = require('gulp-browserify');
+
+gulp.task('dust', function() {
+	return gulp.src('templates/*.dust')
+			.pipe(dust())
+			.pipe(gulp.dest('js/tmpl'));
+});
+
+var bfy_options = {
+	insertGlobals : true,
+	debug : !gulp.env.production
+};
+
+gulp.task('browserify', function() {
+	return gulp.src('js/main.js')
+			.pipe(browserify(bfy_options))
+			.pipe(rename('main.min.js'))
+			.pipe(gulp.dest('js'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('templates/*.dust', ['dust']);
+	gulp.watch('js/main.js', ['browserify']);
+});
